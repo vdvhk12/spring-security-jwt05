9e7b3869aa615682a59b5a5a159fa390cb8eae20
vdvhk12 <vdvhk12@gmail.com>
2025-01-13 02:25:11 +0900

    backend: feat: 프론트에서 Authorization 헤더값에 접근하기위한 설정 추가

--- backend/src/main/java/org/example/backend/global/config/CorsConfig.java
+++ backend/src/main/java/org/example/backend/global/config/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig {
         config.setAllowCredentials(true);   //클라이언트가 자격 증명을 함께 전송할 수 있도록 허용
         config.addAllowedOrigin("http://localhost:3000");    // 허용할 도메인 설정
         config.addAllowedHeader("*");   // 모든 헤더 허용
+        config.addExposedHeader("Authorization");   // 클라이언트에서 헤더에있는 Authorization에 접근가능하도록 하는 설정
         config.addAllowedMethod("*");   // 모든 메소드 허용
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
