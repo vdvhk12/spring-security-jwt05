39426d8c27924f1ce958d647b01f153be8f5a38c
vdvhk12 <vdvhk12@gmail.com>
2025-01-13 12:49:21 +0900

    backend: feat: 액세스 토큰 갱신 컨트롤러에서 리프레시 토큰을 쿠키로 받도록 수정
    - 필요없는 TokenRefreshForm은 삭제

--- backend/src/main/java/org/example/backend/global/auth/controller/AuthController.java
+++ backend/src/main/java/org/example/backend/global/auth/controller/AuthController.java
@@ -1,10 +1,10 @@
 package org.example.backend.global.auth.controller;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.example.backend.global.auth.dto.LoginForm;
-import org.example.backend.global.auth.dto.TokenRefreshForm;
 import org.example.backend.global.auth.service.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -35,11 +35,24 @@ public class AuthController {
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<?> refresh(@RequestBody TokenRefreshForm form, HttpServletResponse response) {
-        String refreshToken = form.getRefreshToken();
-        String token = authService.tokenRefresh(refreshToken);
+    public ResponseEntity<?> refresh(HttpServletRequest request, HttpServletResponse response) {
+        String refreshToken = null;
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null) {
+            for(Cookie cookie : cookies) {
+                if(cookie.getName().equals("refreshToken")) {
+                    refreshToken = cookie.getValue();
+                    break;
+                }
+            }
+        }
+        if(refreshToken == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("리프레시 토큰이 쿠키에 없습니다.");
+        }
+
+        String newAccessToken = authService.tokenRefresh(refreshToken);
         response.addHeader("Authorization",
-            "Bearer " + token);
+            "Bearer " + newAccessToken);
         return ResponseEntity.status(HttpStatus.OK).body("엑세스 토큰 재발급 성공");
     }
 }
