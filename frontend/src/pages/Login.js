19b2bdc4200d47a70af2fae6c210abf6aa7b172a
vdvhk12 <vdvhk12@gmail.com>
2025-01-14 09:21:45 +0900

    frontend: fix: 로그인 후 리프레시 토큰 저장 안되는 문제 해결
    - 로그인 요청시에도 credentials: 'include' 를 붙여 주어야 서버에서 Set-Cookie로 클라이언트에 저장할 수 있음.

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -14,6 +14,7 @@ function Login() {
     try {
       const response = await fetch("http://localhost:8080/auth/login", {
         method: "POST",
+        credentials: 'include',
         headers: {
           "Content-Type": "application/json",
         },
